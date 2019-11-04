dced1f14fcab6937fd5a8d3c5e71065e62ae8436
Yashwant Singh Zala <yashwantsinghzala@gmail.com>
2019-11-05 03:29:31 +0530

    updated file

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 
 const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log("App is listening on port " + port);
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
